7e1db70bab3bc98be3437e2bf315f97a3dba0f74
showstopper728 <bestintheworld728@yahoo.com>
2019-05-03 18:01:50 +0200

    Seventh commit

--- RemusiX-master/app/src/main/java/com/example/abdo/remusix/NotificationFragment.java
+++ RemusiX-master/app/src/main/java/com/example/abdo/remusix/NotificationFragment.java
@@ -7,18 +7,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-
 import java.util.ArrayList;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class NotificationFragment extends Fragment {
    ListView listView;
 
     public NotificationFragment() {
-        // Required empty public constructor
+
     }
 
 
@@ -29,6 +24,12 @@ public class NotificationFragment extends Fragment {
         View v= inflater.inflate(R.layout.fragment_notification, container, false);
         listView = v.findViewById(R.id.notiList);
         ArrayList<notification>arrayList = new ArrayList<>();
+        arrayList.add(new notification(R.drawable.lovly,"Ahmed","shared a post","2m ago"));
+        arrayList.add(new notification(R.drawable.lovly,"Ahmed","shared a post","2m ago"));
+        arrayList.add(new notification(R.drawable.lovly,"Ahmed","shared a post","2m ago"));
+        arrayList.add(new notification(R.drawable.lovly,"Ahmed","shared a post","2m ago"));
+        arrayList.add(new notification(R.drawable.lovly,"Ahmed","shared a post","2m ago"));
+        arrayList.add(new notification(R.drawable.lovly,"Ahmed","shared a post","2m ago"));
         arrayList.add(new notification(R.drawable.lovly,"Ahmed","shared a post","2m ago"));
         notificationAdapter adapter = new notificationAdapter(getContext(),arrayList);
         listView.setAdapter(adapter);
